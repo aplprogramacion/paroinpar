66017c66bfb03039325a27b06ca4aaafa9dc5b55
AntonioDev <aplprogramacion@gmail.com>
2023-03-23 00:56:11 +0100

    reto conseguido

---
+++ main.py
@@ -0,0 +1,8 @@
+# Preguntar el número
+numero = int(input("Por favor, ingresa un número entre 1 y 1000: "))
+
+# Comprobar si es par o impar
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
